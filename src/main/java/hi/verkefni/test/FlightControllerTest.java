0dc06714d71933bfaf88d4c7ff905d7c033163c4
hrolfurj <hrolfur@hi.is>
2022-03-20 17:20:03 +0000

    meira test

--- src/main/java/hi/verkefni/test/FlightControllerTest.java
+++ src/main/java/hi/verkefni/test/FlightControllerTest.java
@@ -1,5 +1,33 @@
 package hi.verkefni.test;
 
+import static org.junit.Assert.*;
+
 class FlightControllerTest {
+
+    private FlightMock mockFlug;
+
+    @Before
+    public void setUp{
+
+        mockFlug = new FlightMock();
+
+    }
+
+    @After
+    public  void tearDown{
+
+        mockFlug = null;
+
+    }
+
+
+    @Test
+    public void testAmount{
+
+        assertEquals("Reykjavík“, mockFlug.getFlight(0).getDeparture());
+
+    }
+
+
 
 }
