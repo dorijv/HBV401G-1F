36b376f318a04876ad2cf93c7aa93624a61a5393
Halldór <hjv6@hi.is>
2022-04-03 18:44:12 +0000

    schema að db

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS Flights CASCADE;
+DROP TABLE IF EXISTS Bookings CASCADE;
+
+CREATE TABLE Flights (
+    FlightID CHAR(6),
+    departureLoc VARCHAR(25),
+    destination VARCHAR(25),
+    departureTime DATETIME,
+    arrivalTime DATETIME,
+    availableSeats INTEGER,
+    price FLOAT,
+    PRIMARY KEY(FlightID, departureTime)
+);
+
+CREATE TABLE Bookings (
+	SSN VARCHAR(10),
+	FlightID CHAR(6),
+    departureTime DATETIME,
+    lastName VARCHAR(30),
+    firstName VARCHAR(30),
+    row INTEGER,
+    seat CHAR(1),
+    confirmationNO INTEGER,
+    FOREIGN KEY(FlightId, departureTime) REFERENCES Flights(FlightID, departureTime),
+    PRIMARY KEY(SSN, FlightId, departureTime)
+);
